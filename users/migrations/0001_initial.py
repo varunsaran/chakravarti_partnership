bb31835a109836cef165e221ba9ea999cd45606a
Varun Saran <varunsaran@berkeley.edu>
2020-01-23 21:37:38 -0800

    fixed null db error

--- users/migrations/0001_initial.py
+++ users/migrations/0001_initial.py
@@ -19,7 +19,7 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('image', models.ImageField(default='default.jpg', upload_to='profile_pics')),
-                ('sheets_id', models.CharField(max_length=15, unique=True)),
+                ('sheets_id', models.CharField(max_length=15, unique=True, blank=True, null=True)),
                 ('user', models.OneToOneField(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
             ],
         ),
