c0e28d78c6dba187f0fb6ef1403ae2de0272164a
Varun Saran <varunsaran@berkeley.edu>
2020-01-23 21:59:26 -0800

    fixed db error for sheets_id

--- users/models.py
+++ users/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
-    sheets_id = models.CharField(max_length=15, unique=True, default='')
+    sheets_id = models.CharField(max_length=15, default='')
 
     def __str__(self):
         return f'{self.user.username} Profile' #pylint: disable=no-member
