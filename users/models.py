ccc4d7a144fa02c8a7480a25eb79cf083c39c5ee
Varun Saran <varunsaran@berkeley.edu>
2020-01-23 21:50:24 -0800

    fixed null db error

--- users/models.py
+++ users/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
-    sheets_id = models.CharField(max_length=15, unique=True, blank=True, null=True)
+    sheets_id = models.CharField(max_length=15, unique=True, default='')
 
     def __str__(self):
         return f'{self.user.username} Profile' #pylint: disable=no-member
